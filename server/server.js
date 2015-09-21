ae6b84e5bb50218f2d25b2174d6e00adac3b2af8
John Anderson <jfanderson373@gmail.com>
2015-09-21 16:28:13 -0700

    Route requests to middleware. Remove temporary test request

--- server/server.js
+++ server/server.js
@@ -7,14 +7,8 @@ var app = express();
 
 mongoose.connect('mongodb://localhost/reinvent');
 
-// route for testing shopify api and db
-app.get('/', function(req, res) {
-  var url = 'https://' + api.key + ':' + api.pw + '@' + api.domain + '/admin/products.json';
-  
-  request(url, function(err, response, body) {
-    res.end(body);
-  });
-});
+// all routing for the app
+require('./middleware.js')(app, express);
 
 app.listen(8000);
 console.log('Server listening on port 8000...');
