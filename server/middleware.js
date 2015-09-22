3c37f471c1eb00a07c1398b7d2aea80e31e84513
John Anderson <jfanderson373@gmail.com>
2015-09-21 22:14:58 -0700

    Add Morgan for request logging

--- server/middleware.js
+++ server/middleware.js
@@ -1,7 +1,9 @@
 var products = require('./products/productRoutes.js');
+var morgan = require('morgan');
 
 module.exports = function(app, express) {
 
+  app.use(morgan('dev')); // request logging
   app.use(express.static(__dirname + '/../client'));
 
   app.use('/products', products);
