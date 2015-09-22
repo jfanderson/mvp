2a0a8d5bd7f17158f06fbca3b85e535b504aaef4
John Anderson <jfanderson373@gmail.com>
2015-09-21 20:04:26 -0700

    Respond to GET with list of products. Async fixes for db updating

--- server/products/productController.js
+++ server/products/productController.js
@@ -1,30 +1,34 @@
+var async = require('async');
 var request = require('request');
 var Q = require('q');
 var Product = require('./productModel.js');
 var api = require('../config.js');
 
 module.exports = {
-  getProducts: function (req, res, next) {
+  getProducts: function(req, res, next) {
     var url = 'https://' + api.key + ':' + api.pw + '@' + api.domain + '/admin/products.json';
     
     // make GET request to shopify for all products
-    request(url, function (error, response, body) {
+    request(url, function(error, response, body) {
       if (error) {
-        console.log('Error getting products: ', error);
+        console.log('Error getting products from shopify: ', error);
       }
-      
-      // promisify Mongo findOne and create functions
+    
+      // promisify Mongoose findOne and create functions
       var findProduct = Q.nbind(Product.findOne, Product);
       var createProduct = Q.nbind(Product.create, Product);
+      
+      var products = JSON.parse(body).products;
 
-      JSON.parse(body).forEach(function (product) {
-        findProduct({title: product.title})
-          .then(function (match) {
+      async.each(products, function(product, callback) {
+        findProduct({_id: product.id})
+          .then(function(match) {
             if (match) {
               // if product already in db, then update quantity
               match.quantity = product.variants[0].inventory_quantity;
+              match.save();
             } else {
-              // if product not in db, then insert it into db
+              // if product not in db, then insert it
               var newProduct = {
                 _id: product.id,
                 title: product.title,
@@ -33,20 +37,26 @@ module.exports = {
                 created_at: product.created_at
               };
 
-              return createProduct(newProduct);
+              createProduct(newProduct);
             }
           })
-          .then(function() {
-            // send all products back in response
-            
-          })
+          .then(callback)
           .catch (function(error) {
             if (error) {
-              console.log('Error updating db: ', error);
+              console.log('Error updating single product entry: ', error);
             }
           });
+      }, function(error) {
+        if (error) {
+          console.log('Error updating products: ', error);
+        }
+
+        // Once db updated, respond with list of products
+        Product.find({}, function(error, result) {
+          res.send(result);
+        });        
       });
-    });
+    });        
   } 
   
 };
