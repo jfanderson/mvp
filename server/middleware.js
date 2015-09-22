436a86f73d804732b39dc6ee0aa0fffc02dafe22
John Anderson <jfanderson373@gmail.com>
2015-09-22 11:10:55 -0700

    Add styles to page

--- server/middleware.js
+++ server/middleware.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 
 module.exports = function(app, express) {
 
-  app.use(morgan('dev')); // request logging
+  // app.use(morgan('dev')); // request logging
   app.use(express.static(__dirname + '/../client'));
 
   app.use('/products', products);
