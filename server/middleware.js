2bd6a095dddcf5d86803d07ea38fbd18e8f3868d
John Anderson <jfanderson373@gmail.com>
2015-09-22 17:01:40 -0700

    Add body-parser to middleware

--- server/middleware.js
+++ server/middleware.js
@@ -1,9 +1,12 @@
 var products = require('./products/productRoutes.js');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 module.exports = function(app, express) {
 
   // app.use(morgan('dev')); // request logging
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../client'));
 
   app.use('/products', products);
